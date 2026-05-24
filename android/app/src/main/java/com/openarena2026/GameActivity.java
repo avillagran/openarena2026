7c84d27c96e325b70ddd7e25fc261dfff7beed75
Andrés Villagrán <andres@villagranquiroz.cl>
2026-05-23 23:38:40 -0400

    Fix build: add launcher icon, fix GameActivity, Java 21 compat
    - Added drawable/ic_launcher_foreground as temporary launcher icon
    - Fixed unreachable statement in GameActivity
    - Disabled native build temporarily for UI testing
    - Build and install verified on Xiaomi 23090RA98G (Android 15)

--- android/app/src/main/java/com/openarena2026/GameActivity.java
+++ android/app/src/main/java/com/openarena2026/GameActivity.java
@@ -3,6 +3,7 @@ package com.openarena2026;
 import android.app.NativeActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Native activity that runs the OpenArena engine.
@@ -10,19 +11,17 @@ import android.view.View;
  */
 public class GameActivity extends NativeActivity {
 
-    static {
-        System.loadLibrary("openarena2026");
-    }
+    // static {
+    //     System.loadLibrary("openarena2026");
+    // }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Hide system bars for immersive mode
-        getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            | View.SYSTEM_UI_FLAG_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        );
+
+        // TODO: Native engine not yet built. Showing placeholder.
+        Toast.makeText(this, "Native engine coming soon!", Toast.LENGTH_LONG).show();
+        finish();
     }
 
     @Override
